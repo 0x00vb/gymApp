87a7fff182a60da8c74be8660bbc407aedee5965
ValentinoBalatti4 <valentino@LAPTOP-IMFAAKK3>
2024-03-21 19:15:43 -0300

    Check DB existance before loading prepopulated

--- utils/db-service.js
+++ utils/db-service.js
@@ -2,21 +2,28 @@ import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
 import { Asset } from 'expo-asset';
 
-async function openDatabase(){
-    if(!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists){
-        await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+async function openDatabase() {
+    const dbDirectory = FileSystem.documentDirectory + 'SQLite/';
+    const dbFilePath = dbDirectory + 'database.db';
+
+    try {
+        const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+        if (!fileInfo.exists) {
+            // Directory or database file doesn't exist, create directory and download
+            await FileSystem.makeDirectoryAsync(dbDirectory, { intermediates: true });
+            await FileSystem.downloadAsync(
+                Asset.fromModule(require("../assets/database.db")).uri,
+                dbFilePath
+            );
+            console.log('Database downloaded to: ', dbFilePath);
+        } else {
+            console.log('Database already exists at: ', dbFilePath);
+        }
+        return SQLite.openDatabase('database.db');
+    } catch (error) {
+        console.log('Error opening database:', error);
+        throw error;
     }
-    await FileSystem.downloadAsync(
-        Asset.fromModule(require("../assets/database.db")).uri,
-        FileSystem.documentDirectory + 'SQLite/database.db'
-    )
-    .then(({ uri }) => {
-        console.log('Downloaded to: ', uri);
-    })
-    .catch( error => {
-        console.log(error)
-    })
-    return SQLite.openDatabase('database.db')
 }
 
 export default openDatabase;
