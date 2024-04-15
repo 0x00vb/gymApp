c6361587f7143adcf0afb171dc1b66d3435682eb
ValentinoBalatti4 <valentinobalatti4@gmail.com>
2024-04-15 00:13:20 -0300

    Pie chart completed

--- components/PieChartGraph.js
+++ components/PieChartGraph.js
@@ -1,58 +1,103 @@
 import {React, useEffect, useState} from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
 import { useSQLiteContext } from "expo-sqlite/next";
-const data = [
-    { name: 'Seoul', population: 21500000, color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-    { name: 'Toronto', population: 2800000, color: '#F00', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-    { name: 'Beijing', population: 527612, color: 'red', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-    { name: 'New York', population: 8538000, color: '#ffffff', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-    { name: 'Moscow', population: 11920000, color: 'rgb(0, 0, 255)', legendFontColor: '#7F7F7F', legendFontSize: 15 }
-]
+
 const PieChartGraph = () => {
+    const [chartData, setChartData] = useState([]);
+    const colors = ["#228cdb", "#4cb1ff", "#aedcff"]
+
+    const db = useSQLiteContext();
+
+    useEffect(() => {
+        const getTopExercises = async () => {
+            try{
+                const response = await db.getAllAsync(`
+                    SELECT e.exercise_name, AVG(l.weights / (1.0278 - 0.0278 * CAST(l.reps AS INTEGER))) AS avg_1rm
+                    FROM Logs l
+                    INNER JOIN Exercises e ON l.exercise_id = e.id
+                    GROUP BY e.exercise_name
+                    ORDER BY avg_1rm DESC
+                    LIMIT 3;
+                `)
+
+                const exercises = response.map((item, index) => ({
+                    name: item.exercise_name,
+                    population: parseFloat(item.avg_1rm.toFixed(2)),
+                    color: colors[index],
+                    legendFontSize: 14
+                }))
+                setChartData(exercises);
+            }catch(e){
+                console.log("Error fetching exercises info: ", e);
+            }
+        }
+        getTopExercises();
+        console.log(chartData)
+    }, [])
+
     return(
         <View>
-        <View>
-            <Text style={{fontSize: 20, fontWeight: '600'}}>
-                Favorite execises
-            </Text>
-        </View>
-            <PieChart
-                data={data}
-                width={Dimensions.get("window").width - 40} // from react-native
-                height={220}
-                hasLegend={true}
-                chartConfig={{
-                    backgroundColor: "#D9D9D9",
-                    backgroundGradientFrom: "#D9D9D9",
-                    backgroundGradientTo: "#F1F1F1",
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => `rgba(33, 126, 194, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(33, 126, 194, ${opacity})`,
-                    style: {
-                        borderRadius: 16
-                    },
-                    propsForDots: {
-                        r: "6",
-                        strokeWidth: "2",
-                        stroke: "#FFFFFF"
-                    }
-                }}       
-                style={{
-                    marginVertical: 8,
-                    borderRadius: 16
-                }}                 
-                accessor="population"
-                backgroundColor="transparent"
-                paddingLeft="15"
-                absolute
-                />  
+            <View>
+                <Text style={{fontSize: 20, fontWeight: '600'}}>
+                    Strongest lifts
+                </Text>
+            </View>
+            <View>
+                {
+                    chartData == [] ? 
+                        <Text>
+                            No data available
+                        </Text>
+                    :
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <PieChart
+                                data={chartData}
+                                width={Dimensions.get("window").width - 40} // from react-native
+                                height={220}
+                                backgroundColor='#d9d9d980'
+                                hasLegend={false}
+                                chartConfig={{
+                                    decimalPlaces: 2,
+                                    color: (opacity = 1) => `rgba(33, 126, 194, ${opacity})`,
+                                    style: {
+                                        borderRadius: 16
+                                    },
+                                    propsForDots: {
+                                        r: "6",
+                                        strokeWidth: "2",
+                                        stroke: "#FFFFFF"
+                                    }
+                                }}       
+                                style={{
+                                    marginVertical: 8,
+                                    borderRadius: 16,
+                                }}                 
+                                accessor={"population"}
+                                paddingLeft="15"
+                                absolute
+                            /> 
+                            <View style={{right: '60%', gap: 20}}>
+                            {
+                                chartData.map((item, index) => (
+                                    <View style={{flexDirection: 'row', alignItems: 'center', gap: 10}} key={index}>
+                                        <View
+                                            style={[{width: 20, height: 20, borderRadius: "50%"}, {backgroundColor: item.color}]}
+                                        ></View>
+                                        <View style={{alignItems: 'center'}}>
+                                            <Text style={{fontSize: 14}}>{item.name}</Text>
+                                            <Text>{item.population}Kg</Text>
+                                        </View>
+                                    </View>
+
+                                ))
+                            }
+                            </View>
+                        </View>
+                }
+            </View>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-
-})
 
 export default PieChartGraph;
